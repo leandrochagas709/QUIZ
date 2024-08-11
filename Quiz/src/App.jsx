bf37cd5ae7b1891c9c012e33df2f2d0b7ea9dc71
Leandro Chagas <65906391+leandrochagas709@users.noreply.github.com>
2024-08-11 18:47:44 -0300

    Add files via upload

---
+++ Quiz/src/App.jsx
@@ -0,0 +1,14 @@
+import Welcome from './components/Welcome';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Quiz de Programação</h1>
+        <Welcome />
+    </div>
+  )
+}
+
+export default App;
