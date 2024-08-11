ed29464ac454eef865504f7b1410c7e32da07400
Leandro Chagas <65906391+leandrochagas709@users.noreply.github.com>
2024-08-11 18:51:20 -0300

    Delete Quiz directory

--- Quiz/src/context/quiz.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const QuizContext = createContext()
-
-export const QuizProvider = ({children}) => {
-    return <QuizContext.Provider>{children}</QuizContext.Provider>;
-};
